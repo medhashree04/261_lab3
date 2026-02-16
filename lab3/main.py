a9570ca68357245eae70c90d2759b72814d8029b
jana <jana.vadillo1@gmail.com>
2026-02-16 11:21:49 -0600

    added in base functions

--- lab3/main.py
+++ lab3/main.py
@@ -4,8 +4,8 @@
 # Acknowledgements: 
 
 # Packages
-import numpy
-import matplotlib
+import numpy as np
+import matplotlib.pyplot as plt
 
 gamma = 0.9
 T = 0
@@ -13,11 +13,35 @@ P = 0
 alpha = [0.1, 1/T, 0.1, 1/T, 0.1 ,1/P]
 explore = [0.25, 1/T, 1/T, 0.1, 0.1, 0.1]
 n_sims = 1000
-#given by assignment
+##learning algorithm variables 
+init_state = 12
+terminal_states = [0, 15]
 
+states = np.arange(16)
+#States 
+R_move = -1
+R_terminal = 0
+#Rewards
 
 n_episodes = 10
 #set by us
+def update_Q(S, ):
+    print('implement')
+
+def pick_move():
+    print('implement')
+
+def run_episode():
+    T+=1
+
+def run_simulation():
+    for episode in range(n_episodes):
+        
+
+
+
+
+
 
 def main():
     print("AI Lab 3")
