e51df1dfeb54c4bc34550f28ec0ab388e2f72d9c
jana <jana.vadillo1@gmail.com>
2026-02-16 11:08:21 -0600

    added in global variables for experiment parameters

--- lab3/main.py
+++ lab3/main.py
@@ -7,6 +7,18 @@
 import numpy
 import matplotlib
 
+gamma = 0.9
+T = 0
+P = 0
+alpha = [0.1, 1/T, 0.1, 1/T, 0.1 ,1/P]
+explore = [0.25, 1/T, 1/T, 0.1, 0.1, 0.1]
+n_sims = 1000
+#given by assignment
+
+
+n_episodes = 10
+#set by us
+
 def main():
     print("AI Lab 3")
 
