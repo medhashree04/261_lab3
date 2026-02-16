cb2af5fe86617e5a0462b7aedf7dd3bd4fe9c654
Medhashree Adhikari <adhikari3@grinnell.edu>
2026-02-16 09:19:38 -0600

    Created Lab 3

---
+++ lab3/main.py
@@ -0,0 +1,12 @@
+# Group: Medhashree Adhikari, Jana Vadillo, Bonsen Yusuf
+# Due Date: Monday, February 28, 2026
+#
+# Acknowledgements: 
+
+# Packages
+import numpy
+
+def main():
+    print("AI Lab 3")
+
+main()
